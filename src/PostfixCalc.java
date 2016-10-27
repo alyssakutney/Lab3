5b1fb1a516bc5e611e2053b468431d41f8f79549
alyssakutney <alyssakutney@gmail.com>
2016-10-27 18:23:07 -0400

    commit update 2

--- src/PostfixCalc.java
+++ src/PostfixCalc.java
@@ -1,21 +1,42 @@
 package lab3;
+import java.util.ArrayList;
 /**
- * 
+ * The PostfixCalc class will provide the user with a Postfix Calculator that will
+ * turn their input expression of an infix expression and change it to a postfix expression.
  * @author kutneya1
  *
  */
 public class PostfixCalc {
-	
+	private String expression;
 	
 	public PostfixCalc(expression){
-	this.expression = expression;
-  	}
+    this.expression = expression;
+  }
 	
 	
 	public static void main(String[] args){
 		
 	}
 	
+  public void push(String input){
+	if(topOfStack + 1 < stackSize){
+	  topOfStack++;
+	  stackArray[topOfStack] = input;
+ 	}
+  }
+  
+  public String pop(){
+	if(topOfStack >= 0 ){
+	  displayStack();
+	  System.out.println();
+	}
+  }
+  
+  public void displayStack(){
+	for(int i = 0; i < stackSize; i++){
+	  System.out.println(i);
+	}
+  }
 	
 	
 }
